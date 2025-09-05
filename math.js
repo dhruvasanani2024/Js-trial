dc8b5c33bce94624dd0f9af4927efaa1e068479d
dhruvasanani2024 <160484227+dhruvasanani2024@users.noreply.github.com>
2025-09-05 14:31:20 +0000

    new

---
+++ math.js
@@ -0,0 +1,8 @@
+let x = Math.random();
+
+console.log(Math.floor((x*10) + 1));
+
+let min = 10 
+let max = 20
+
+console.log(Math.round((Math.random()*(max-min + 1))+min))
---
+++ strings.js
@@ -0,0 +1,14 @@
+const name = "Hello"
+const b = "bro"
+console.log(`${name} there ${b}`)
+
+const str = new String('Hello')
+
+console.log(`${str}`)
+
+const g = "     Hi Bro    "
+
+const url = "a;skjdfhaskdjfaskdjfhasdkl;jfhslk"
+console.log(g.trim())
+
+console.log(url.replace(';','*******'))
